54e3745d75023d8fd8d2c7a7856bee8b2a603d6a
Christopher Danella <christopher.danella@gmail.com>
2016-04-13 09:57:11 -0400

    Add case statement

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,12 +1,16 @@
+puts "How many employees will be processed?"
+num_employees = gets.chomp
+
 puts "What is your name?"
 name = gets.chomp
 
 puts "How old are you? What year were you born?"
 age = gets.chomp
 year = gets.chomp
-puts age.to_i + year.to_i
 real_age = age.to_i + year.to_i
+puts real_age
 current_year = 2016
+puts current_year
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you?"
 garlic_preference = gets.chomp
@@ -16,13 +20,13 @@ insurance_preference = gets.chomp
 
 
 if real_age == current_year && garlic_preference == "yes" && insurance_preference == "yes"
-	puts "Probably not a vampire."
-elsif real_age != current_year && garlic_preference == "no" || insurance_preference == "no"
-	puts "Probably a vampire."
-elsif real_age != current_year && garlic_preference == "no" && insurance_preference == "no"
-	puts "Almot certainly a vampire."
+  puts "Probably not a vampire."
+elsif real_age == current_year && garlic_preference == "no" || insurance_preference == "no"
+  puts "Probably a vampire."
+elsif real_age == current_year && garlic_preference == "no" && insurance_preference == "no"
+  puts "Almot certainly a vampire."
 else
-	puts "Results inconclusive."
+  puts "Results inconclusive."
 end
 
 case name
@@ -32,12 +36,19 @@ end
 
 puts "Your name is #{name}, your age is #{age}."
 if garlic_preference == "yes"
-	puts "Your order of garlic is coming right up."
+  puts "Your order of garlic is coming right up."
 else
-	puts "You declined the garlic offer."
+  puts "You declined the garlic offer."
 end
+
 if insurance_preference == "yes"
-	puts "You are opted into our health insurance."
+  puts "You are opted into our health insurance."
 else
-	puts "You have opted out of our insurance."
+  puts "You have opted out of our insurance."
 end
+
+num_employees = false
+
+until num_employees
+	
+
