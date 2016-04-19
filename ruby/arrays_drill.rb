579371dd427c8695780079d4c7f00d010749c7d6
Christopher Danella <christopher.danella@gmail.com>
2016-04-19 13:34:00 -0400

    Finish assignment-5.2

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,3 +1,15 @@
+def random_method(arr, value)
+  new_array = arr << value
+end
+p random_method(["a", "b"], 4)
+p random_method(["Chris", "Hayden", "Dev Bootcamp"], 88)
+
+def method(boolean, string, integer)
+  answer = [boolean, string, integer]
+  p answer
+end
+method(true, "Dev Bootcamp", 55)
+
 new_array = []
 new_array = ["ruby", 9, true, "DBC", false]
 new_array = []
