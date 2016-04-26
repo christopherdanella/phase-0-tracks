0ce2abc260766f527e420218388847589555d463
Christopher Danella <christopher.danella@gmail.com>
2016-04-26 10:29:42 -0400

    Complete puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,17 @@
+# SPECIES ------------------
+# Canine
+
+# CHARACTERISTICS --------------
+# Color: varies
+# Friendly: yes
+# Leg count: 4
+# Tail: varies
+# Name: varies
+# Ear count: 2
+
+# BEHAVIOR ---------------
+# Bark
+# Run
+# Sleep
+# Jump
+# Cuddle
