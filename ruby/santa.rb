d6f9c10d62f7f11c13ea286bd9eea2be0712e98d
Christopher Danella <christopher.danella@gmail.com>
2016-04-27 12:07:22 -0400

    Finish release 3

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -7,6 +7,9 @@
 
 class Santa
 
+  attr_reader :age, :ethnicity
+  attr_accessor :gender
+
   def initialize(gender, ethnicity)
     @gender = gender
     @ethnicity = ethnicity
@@ -23,34 +26,51 @@ class Santa
     snack
   end
 
+# celebrate_birthday should age Santa by one year
+  def celebrate_birthday(age)
+    @age = age
+    age = age + 1
+  end
+
+# get_mad_at takes a reindeer's name as an argument and moves it to last in reindeer_rankings
+# when reindeer is called by name, it moves to last place in the array
+  def get_mad_at(reindeer_name)
+    @reindeer_ranking.delete(reindeer_name)
+    @reindeer_ranking.push(reindeer_name)
+  end
 
 end
 
 santa = Santa.new("male", "asian")
 santa.speak
 santa.eat_milk_and_cookies("snickerdoodle")
-
+puts santa.celebrate_birthday(40)
+puts santa.get_mad_at("Rudolph")
+santa.gender = "undefined"
+puts "Santa is an #{santa.gender} gender"
+puts santa.age
+puts santa.ethnicity
 
 # container for santa collection
-santas = []
-
-# array of possible genders for santas
-genders = ["male", "female", "transgender", "he-she-it", "N/A"]
-
-#array of possible ethnicities for santas
-ethnicities = ["black", "white", "aboriginal", "extra-terrestrial"]
-
-puts "Iterating through genders list to add for Santas..."
-genders.each do |gender|
-  puts "Adding a gender called #{gender}..."
-  santas << Santa.new(gender, ethnicities)
-  puts "There are now #{santas.length} Santa instances in the array"
-end
-
-puts "Testing..."
-santas.each do |talk|
-  talk.speak
-end
+# santas = []
+# 
+# # array of possible genders for santas
+# genders = ["male", "female", "transgender", "he-she-it", "N/A"]
+# 
+# #array of possible ethnicities for santas
+# ethnicities = ["black", "white", "aboriginal", "extra-terrestrial"]
+# 
+# puts "Iterating through genders list to add for Santas..."
+# genders.each do |gender|
+#   puts "Adding a gender called #{gender}..."
+#   santas << Santa.new(gender, ethnicities)
+#   puts "There are now #{santas.length} Santa instances in the array"
+# end
+# 
+# puts "Testing..."
+# santas.each do |talk|
+#   talk.speak
+# end
 
 
 
