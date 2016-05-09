2bd2473a8357979a5395e38c742ac7f1e003785a
Christopher Danella <christopher.danella@gmail.com>
2016-05-08 22:00:03 -0400

    Finish release 2

--- js/algos.js
+++ js/algos.js
@@ -16,7 +16,7 @@
     // return variable longest
   
 
-  function longest_word(words) {
+  function longestWord(words) {
     var longest = ""
     for (var i = 0; i < words.length; i++) {
       if (words[i].length > longest.length){
@@ -49,7 +49,7 @@
 
 
 
-function match_finder(objOne, objTwo) {
+function matchFinder(objOne, objTwo) {
   for (var key in objOne) {
     if (objTwo.hasOwnProperty(key)) {
       if (objOne[key] === objTwo[key]) {
@@ -66,16 +66,59 @@ function match_finder(objOne, objTwo) {
 }
 
 
+// Release 2: Generate Random Test Data
+// write a function that takes an integer for length and builds
+// and returns an array of strings of the given length. so if we ran
+// your function with an argument of 3, we would get an array of 3
+// random words back. The words should be of randomly varying length,
+// with a minimum of 1 letter and a max of 10 letters.
+// Add driver code that does the following 10 times:
+  // Generates an array, prints the array, feeds the array to your
+  // "longest word" function, and prints the result.
+
+// PSEUDOCODE
+  // write function randomData and give it a number as a parameter.
+  // add two more variables, randomArray as an empty array
+  // randomLetters will be an array of all of the letters of the alphabet
+  // loop through the parameter of the function 
+  // set new variable called randomNumber and use Math.random to generate a random number
+  // use Math.round to make sure that the number is rounded down from a float to a whole number.
+  // declare another variable and set it equal to an empty string.
+  // loop through the randomNumber variable, set the index equal to something other than "i"
+  // push the empty string variable to our randomArray and return randomArray
+  // print it to the console so the user sees the 10 arrays that have been generated.
+
+
+function randomData(number) {
+  var randomArray = [];
+  var randomLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q",
+  "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  for (var i = 0; i < number; i++) {
+    var randomNumber = Math.round(Math.random() * 10) + 1;
+    var word = "";
+    for (var j = 0; j < randomNumber; j++) {
+      word = word + randomLetters[j]
+    }
+    randomArray.push(word)
+  }
+  console.log(randomArray)
+  return randomArray
+}
+
+
 // DRIVER CODE
 
-console.log(longest_word(["Christopher", "Coeli", "Brendan"]))
-console.log(longest_word(["Brooklyn", "Philadelphia", "Phoenix"]))
-console.log(longest_word(["Bill Calderone", "John Smith", "Davey Jones", "Rocky Balboa"]))
+// console.log(longest_word(["Christopher", "Coeli", "Brendan"]))
+// console.log(longest_word(["Brooklyn", "Philadelphia", "Phoenix"]))
+// console.log(longest_word(["Bill Calderone", "John Smith", "Davey Jones", "Rocky Balboa"]))
+// 
+// 
+// 
+// match_finder({name: "Chris", age: 32}, {name: "James", age: 32})
+// match_finder({name: "Wyatt", hair: "brown"}, {name: "Joe", hair: "black"})
 
 
-
-match_finder({name: "Chris", age: 32}, {name: "James", age: 32})
-match_finder({name: "Wyatt", hair: "brown"}, {name: "Joe", hair: "black"})
+console.log(longestWord(randomData(10)))
 
 
 
