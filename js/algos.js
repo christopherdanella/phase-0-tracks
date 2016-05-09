195210928abca0c5c9e572bb99f3a9f5f7007ab0
Christopher Danella <christopher.danella@gmail.com>
2016-05-08 20:54:19 -0400

    Finish Release 1

--- js/algos.js
+++ js/algos.js
@@ -12,7 +12,7 @@
     // set new variable called longest to an empty string
     // use conditional IF statement to check if array words index is greater
     // than the length of variable longest
-    // IF true then varable longest is equal to index of array words.
+    // IF true then variable longest is equal to index of array words.
     // return variable longest
   
 
@@ -26,10 +26,56 @@
     return longest;
   }
 
+// Release 1: Find a Key-Value Match
+// Write a function that takes two objects and checks to see if they
+// share at least one key-value pair
+// If we called your function with {name: "Steven", age: 54} and {name: "Tamir", age: 54} 
+// the function would return true because at least one key-value pair matches between the two objects.
+// If no pairs match (and keep in mind that the two objects may not even have any of the same keys) 
+// the function should return false. To make your life easier, 
+// don't worry about whether a property is a string ('age') or an identifier name (age). 
+// Those can be considered equivalent.
+
+
+// PSEUDOCODE
+  // name function match_finder using objOne and objTwo as parameters
+  // create a variable called "key" and loop through objOne
+  // use hasOwnProperty function in javascript to loop through parameters
+  // and figure out if there is a match in the keys.
+  // use conditional IF statement to see if objOne and objTwo have keys
+  // that are equal to each other
+  // IF there is a match, print true
+  // ELSE print false
+
+
+
+function match_finder(objOne, objTwo) {
+  for (var key in objOne) {
+    if (objTwo.hasOwnProperty(key)) {
+      if (objOne[key] === objTwo[key]) {
+        var match = objOne[key];
+      }
+    }
+  }
+
+  if (match) {
+    console.log("True, the match is " + match);
+  } else { 
+    console.log("False, there is no match.")
+  }
+}
+
+
+// DRIVER CODE
+
 console.log(longest_word(["Christopher", "Coeli", "Brendan"]))
 console.log(longest_word(["Brooklyn", "Philadelphia", "Phoenix"]))
 console.log(longest_word(["Bill Calderone", "John Smith", "Davey Jones", "Rocky Balboa"]))
 
+
+
+match_finder({name: "Chris", age: 32}, {name: "James", age: 32})
+match_finder({name: "Wyatt", hair: "brown"}, {name: "Joe", hair: "black"})
 
 
 
