56a4d5afc7b5e5cc112d4c983c8128e5228c402a
Christopher Danella <christopher.danella@gmail.com>
2016-05-12 12:15:10 -0400

    Finish release 1 of array drills

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,26 +1,26 @@
-def random_method(arr, value)
-  new_array = arr << value
-end
-p random_method(["a", "b"], 4)
-p random_method(["Chris", "Hayden", "Dev Bootcamp"], 88)
-
-def method(boolean, string, integer)
-  answer = [boolean, string, integer]
-  p answer
-end
-method(true, "Dev Bootcamp", 55)
-
 new_array = []
-new_array = ["ruby", 9, true, "DBC", false]
-new_array = []
-new_array = ["ruby", 9, true, "DBC", false]
-new_array.delete_at(2)
-new_array.insert(2, "NYC")
-new_array.shift
-new_array.include?("DBC")
 p new_array
 
-second_array = ["Chris", "Hayden", 38]
+new_array << "item 1"
+new_array << "item 2"
+new_array << 3
+new_array << "item 4"
+new_array << true
+p new_array
 
-third_array = new_array + second_array
-p third_array
+
+new_array.delete_at(2)
+p new_array
+
+new_array.insert(2, "Lily")
+p new_array
+
+new_array.delete("item 1")
+p new_array
+
+puts "The array includes item 2 = #{new_array.include?("item 2")}"
+
+second_array = ["Chris", "Adam", false, "Brooklyn"]
+
+combined_arrays = new_array + second_array
+p combined_arrays
