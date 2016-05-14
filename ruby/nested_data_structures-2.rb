bca317d6c2bf6d08a7956df51f3ac08dd7f4b007
Christopher Danella <christopher.danella@gmail.com>
2016-05-14 14:28:25 -0400

    Create nested data structure

---
+++ ruby/nested_data_structures-2.rb
@@ -0,0 +1,46 @@
+library = {
+  fiction: {
+    book_info: {
+      total_books: 750,
+      average_price: 12
+    },
+    bestselling_authors: [
+      "Ernest Hemingway",
+      "Jack London",
+      "Michael Chabon",
+      "John Le Carre"
+    ]
+  },
+  non_fiction: {
+    book_info: {
+      total_books: 1000,
+      average_price: 15
+    },
+    bestselling_authors: [
+      "Noam Chomsky",
+      "Erik Larson",
+      "Howard Zinn"
+    ]
+  },
+  cookbooks: {
+    book_info: {
+      total_books: 375,
+      average_price: 27
+    },
+    bestselling_authors: [
+      "Ina Garten",
+      "Padma Lakshmi",
+      "John Besh",
+      "Anthony Bourdain"
+    ]
+  },
+  travel: {
+    book_info: {
+      total_books: 200,
+      average_price: 17
+    },
+    bestselling_authors: []
+  }
+}
+
+
