b0622fad45242eca1ca148e32e1e8278c6c7a9ba
Christopher Danella <christopher.danella@gmail.com>
2016-05-16 10:51:34 -0400

    Finish user interface and store input in a data structure

--- ruby/alias_manager-2.rb
+++ ruby/alias_manager-2.rb
@@ -9,27 +9,16 @@
 
 
 
-# use .SPLIT to turn the name parameter into an array
-
-# IF character is a space, it should be skipped
-
-
-
-# create consonant array containing all consonants
-consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q",
-              "r", "s", "t", "v", "w", "x", "y", "z"]
-
-
 # create method called name_generator
 # add parameter called (name)
-
 def name_generator(name)
   # use .downcase to make sure all letters are lowercase 
-  # reverse the name so last name appears first
-  name.downcase.split('').reverse!
+  # reverse the name so last name appears first, join them back
+  # together after they've been reversed
+  name.downcase.split(' ').reverse.join(' ')
 end
 
-p name_generator("Felicia Torres")
+# p name_generator("Felicia Torres")
 
 def next_vowel(string)
 # create a vowels variable containing letters "aeiou"
@@ -44,10 +33,10 @@ def next_vowel(string)
 # create a new vowel variable and set it to an empty string
   new_vowel = ""
 
-#loop through the array of letters
+# loop through the array of letters
   letters.each do |letter|
-  #if the letter is a vowel, replace it with the next vowel
-  #in the sequence
+  # if the letter is a vowel, replace it with the next vowel
+  # in the sequence
     if vowels.include?(letter)
       new_vowel += conversions[letter]
     else
@@ -57,7 +46,7 @@ def next_vowel(string)
     end
   new_vowel
 end
-puts next_vowel("abcdefiou")
+# puts next_vowel("abcdefiou")
 
 def next_consonant(string)
   consonants = "bcdfghjklmnpqrstvwxyz"
@@ -68,7 +57,7 @@ def next_consonant(string)
     "l" => "m", "m" => "n", "n" => "p", "p" => "q",
     "q" => "r", "r" => "s", "s" => "t", "t" => "v",
     "v" => "w", "w" => "x", "x" => "y", "y" => "z",
-    "z" => "b"}
+    "z" => "b" }
 
   letters_cons = string.split('')
 
@@ -84,6 +73,38 @@ def next_consonant(string)
   new_consonant
 end
 
-puts next_consonant("bcdfghijz")
+# puts next_consonant("bcdfghijz")
 
+def alias_creator(agent_name)
+  # reverse the names so the last name appears first
+  reversed_name = name_generator(agent_name)
+  # make sure the vowels are concentrated and shifted to the next vowel
+  vowel_switcher = next_vowel(reversed_name)
+  # same with consonants using method above
+  cons_switcher = next_consonant(vowel_switcher)
+  # combine everything
+  cons_switcher.split(' ').map { |name| name.capitalize }.join(' ')
+end
+p alias_creator("Felicia Torres")
 
+# USER INTERFACE
+
+fake_names = {}
+
+loop do
+puts "Welcome to the alias generator"
+puts "Please enter a name, type 'quit' when you are finished."
+spy_name = gets.chomp
+
+if spy_name == "quit"
+  break
+end
+
+puts alias_creator(spy_name)
+fake_names[spy_name] = alias_creator(spy_name)
+end
+
+puts "Here are all of the spies and their names:"
+fake_names.each do |key, value| 
+  puts "#{key} => #{value}"
+end
