804d63bd00b399a09c5bff8619be8eb3eecde710
Christopher Danella <christopher.danella@gmail.com>
2016-05-17 16:51:40 -0400

    Design book class in pseudocode

---
+++ ruby/solo2.rb
@@ -0,0 +1,18 @@
+# PSEUDOCODE
+
+# CLASS -------------------
+  # book
+
+# ATTRIBUTES --------------
+
+  # year_published
+  # color
+  # genre
+
+# METHODS -----------------
+
+  # turn_page
+  # read -> takes number of pages as an argument
+  # close
+
+
