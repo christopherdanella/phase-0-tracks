b2797904f6c8df88f88cc0410efc04b056f8bae8
Christopher Danella <christopher.danella@gmail.com>
2016-05-17 12:29:41 -0400

    Add getter and setter methods to santa class

--- ruby/santa2.rb
+++ ruby/santa2.rb
@@ -17,17 +17,47 @@ class Santa
     puts "That was a good #{cookie}!"
   end
 
+  def celebrate_birthday
+    @age += 1
+  end
+
+  def get_mad_at(reindeer_name)
+    @reindeer_ranking.delete(reindeer_name)
+    @reindeer_ranking << reindeer_name
+  end
+
+  def gender=(new_gender)
+    @gender = new_gender
+  end
+
+  def age
+    @age
+  end
+
+  def ethnicity
+    @ethnicity
+  end
+
+  def gender
+    @gender
+  end
 end
 
-# santa = Santa.new
-# santa.speak
-# santa.milk_and_cookies("oreo")
+santa = Santa.new("male", "red")
+santa.speak
+santa.milk_and_cookies("oreo")
+p santa.celebrate_birthday
+p santa.get_mad_at("Vixen")
+santa.gender = "Extra Terrestrial"
+puts "Santa is now an #{santa.gender}."
+p santa.age
+p santa.ethnicity
 
-santas = []
-possible_genders = ["male", "female", "transgender", "he-she-it"]
-possible_ethnicities = ["black", "brown", "orange", "Asian"]
-
-possible_genders.length.times do |i|
-  santas << Santa.new(possible_genders.sample, possible_ethnicities.sample)
-end
-p santas
+# santas = []
+# possible_genders = ["male", "female", "transgender", "he-she-it"]
+# possible_ethnicities = ["black", "brown", "orange", "Asian"]
+# 
+# possible_genders.length.times do |i|
+#   santas << Santa.new(possible_genders.sample, possible_ethnicities.sample)
+# end
+# p santas
