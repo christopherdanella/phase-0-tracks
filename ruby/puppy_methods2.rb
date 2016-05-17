b487ff7ee8acd4a60024f85870a6eec2d522c919
Christopher Danella <christopher.danella@gmail.com>
2016-05-17 10:41:06 -0400

    Add instance methods to complete release 1

---
+++ ruby/puppy_methods2.rb
@@ -0,0 +1,33 @@
+class Puppy
+
+  def fetch(toy)
+    puts "I brought back the #{toy}!"
+    toy
+  end
+
+  def speak(integer)
+    puts "Woof " * integer
+    integer
+  end
+
+  def roll_over
+    "*rolls over*"
+  end
+
+  def dog_years(age)
+    dog_years = age * 7
+  end
+
+
+  def play_dead
+    "*plays dead*"
+  end
+
+end
+
+puppy = Puppy.new
+p puppy.fetch("ball")
+p puppy.speak(3)
+p puppy.roll_over
+p puppy.dog_years(4)
+p puppy.play_dead
