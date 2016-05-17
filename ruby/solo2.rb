0d946b190b9960ac49cd62037995ddc4305af58a
Christopher Danella <christopher.danella@gmail.com>
2016-05-17 17:56:20 -0400

    Use book class in a program

--- ruby/solo2.rb
+++ ruby/solo2.rb
@@ -20,8 +20,7 @@ class Book
   attr_reader :year_published
   attr_accessor :color, :genre
 
-  def initialize(year_published, genre)
-    puts "Initializing Book instance..."
+  def initialize(year_published)
     @year_published = year_published
     @color = color
     @genre = genre
@@ -40,10 +39,44 @@ class Book
   end
 end
 
-book = Book.new(688, "History")
+book = Book.new("History")
 book.turn_page(244)
 book.read(25)
 book.close
+book.color = "orange"
+puts "This book is now #{book.color}"
+
+# USER INTERFACE
+
+book_collection = []
+
+puts "Greetings, welcome to the book creator."
+loop do 
+puts "When you are finished creating your book(s), please type 'done'."
+puts "What year was your book published?"
+new_book_creator = gets.chomp
+if new_book_creator == "done"
+  break
+end
+puts "What color is your book?"
+color_creator = gets.chomp
+
+puts "What genre is the book?"
+genre_creator = gets.chomp
+
+new_book = Book.new(new_book_creator)
+new_book.color = color_creator
+new_book.genre = genre_creator
+
+book_collection << new_book
+end
+
+book_collection.each do |books|
+  puts "Here are the books you've created: "
+  puts "Year published: #{books.year_published}, color: #{books.color}, genre: #{books.genre}"
+end
+
+
 
 
 
