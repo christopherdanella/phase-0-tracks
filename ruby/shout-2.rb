49b2084cafab5741e702103af268182915654e72
Christopher Danella <christopher.danella@gmail.com>
2016-05-19 11:40:57 -0400

    Add a mixin shout module with two module methods

--- ruby/shout-2.rb
+++ ruby/shout-2.rb
@@ -1,14 +1,45 @@
+# module Shout
+# 
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+# 
+# 
+#   def self.yelling_happily(happy_words)
+#     happy_words + "!" + " :D"
+#   end
+# end
+# 
+# p Shout.yell_angrily("WHAT ARE YOU DOING?")
+# p Shout.yelling_happily("Great job")
+
+
 module Shout
 
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+  def surprise_shout(words)
+    words + "!!! " + "Happy Birthday!"
   end
 
-
-  def self.yelling_happily(happy_words)
-    happy_words + "!" + " :D"
+  def shocked_shout(words)
+    words + "! " + "WHAT HAPPENED?"
   end
 end
 
-p Shout.yell_angrily("WHAT ARE YOU DOING?")
-p Shout.yelling_happily("Great job")
+class Friend
+  include Shout
+end
+
+
+class Sibling
+  include Shout
+end
+
+friend = Friend.new
+p friend.surprise_shout("SURPRISE CHRIS")
+p friend.shocked_shout("Oh my god")
+
+sibling = Sibling.new
+p sibling.surprise_shout("SURPRISE")
+p sibling.shocked_shout("What's going on here?")
+
+
