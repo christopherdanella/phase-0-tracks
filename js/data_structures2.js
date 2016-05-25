f89572fc832e9700061e8046b51dca4395bcb17b
Christopher Danella <christopher.danella@gmail.com>
2016-05-25 12:07:50 -0400

    Add a new color and horse name to the arrays

---
+++ js/data_structures2.js
@@ -0,0 +1,8 @@
+var colors = ["blue", "green", "red", "orange"]
+var horses = ["Ed", "Captain", "Seabiscuit", "Cigar"]
+
+colors.push("black");
+horses.push("Epona");
+
+console.log(colors);
+console.log(horses);
