19516f7b606da75b8f27fc4ccf13c64dcb6d3923
Christopher Danella <christopher.danella@gmail.com>
2016-06-07 14:54:48 -0400

    Add HTML elements using JS

---
+++ web_dev/client_side_js/script.js
@@ -0,0 +1,9 @@
+var addNewElement = document.getElementById("rosheader");
+addNewElement.insertAdjacentHTML('beforebegin', '<div><h1>Insert new content here</h1></div>');
+
+
+var addParagraph = document.getElementById("forwards");
+addParagraph.insertAdjacentHTML('afterend', '<div><p>New paragraph underneath the forwards id!</p></div>');
+
+var idModifier = document.getElementById("defense");
+defense.insertAdjacentHTML('afterbegin', '<div><p><strong>Defense wins championships!</strong></p></div');
