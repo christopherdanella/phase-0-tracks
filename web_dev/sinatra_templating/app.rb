23fe31ffc08be07739454c6d7d06c356fdfe504b
Christopher Danella <christopher.danella@gmail.com>
2016-06-09 13:11:26 -0400

    Add form to create new DBC campus

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -7,6 +7,18 @@ set :public_folder, File.dirname(__FILE__) + '/static'
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
 
+# add campus table to db
+create_table_cmd = <<-SQL
+  CREATE TABLE IF NOT EXISTS campus(
+    id INTEGER PRIMARY KEY,
+    campus_name VARCHAR(255)
+  )
+SQL
+
+#create new table in students database
+db.execute(create_table_cmd)
+
+
 # show students on the home page
 get '/' do
   @students = db.execute("SELECT * FROM students")
@@ -29,6 +41,17 @@ end
 get '/campus' do
   @students = db.execute("SELECT name, campus FROM students")
   erb :campus
+end
+
+# create new campus via
+# a form
+get '/campus/new' do
+  erb :new_campus
+end
+
+post '/students' do
+  db.execute("INSERT INTO campus (campus_name) VALUES (?)", [params['campus_name']])
+  redirect '/'
 end
 
 
